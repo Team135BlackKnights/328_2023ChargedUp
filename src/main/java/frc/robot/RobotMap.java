83e46bb1b0774efc869c52212c50d487daf56ed3
Ttemeles <r.bonnell005@gmail.com>
2023-02-08 14:54:08 -0500

    Update motor IDs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,17 +2,17 @@ package frc.robot;
 public interface RobotMap {
     public interface Drive {
         public final static int 
-           FL_ID = 0,
-           FR_ID = 1,
-           BL_ID = 2,
-           BR_ID = 3;
+           FL_ID = 10,
+           FR_ID = 11,
+           BL_ID = 12,
+           BR_ID = 13;
     }
 
         public interface Intake {
             public final static int
-          INTL_ID = 10,
-          INTR_ID = 9,
-          INTU_ID = 8;  
+          INTL_ID = 22,
+          INTR_ID = 21,
+          INTU_ID = 20;  
     }
 }
 
