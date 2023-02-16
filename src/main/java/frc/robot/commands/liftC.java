d458ed06bd1897d05196509f64846f4704106ee2
coocoo <112586798+coocoo752@users.noreply.github.com>
2023-02-16 15:39:18 -0500

    fix

--- src/main/java/frc/robot/commands/liftC.java
+++ src/main/java/frc/robot/commands/liftC.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 import frc.robot.subsystems.liftS;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.robotContainer;
 public class liftC extends CommandBase {
     public final liftS lift;
@@ -14,13 +13,14 @@ public void initialize() {
 }
 public void execute(){
 
-    if (robotContainer.ManipControl.getAButtonPressed() && liftS.tooFarOut.get()) {
+    if (robotContainer.ManipControl.getAButtonPressed() == true) {
         liftS.flipMotor.set(-.4);
-        
     }
-    else if (robotContainer.ManipControl.getYButtonPressed() && liftS.tooFarIn.get()) {
+    else if (robotContainer.ManipControl.getYButtonPressed() == true ) {
         liftS.flipMotor.set(.4);
     }
-    SmartDashboard.putNumber("liftEncPos", liftS.flipMotorEncoder.getPosition());
+    else {
+        liftS.flipMotor.set(0);
+    }
 }
 }
