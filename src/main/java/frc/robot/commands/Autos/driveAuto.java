8a6b41996cbe265f09546ed70436094b5e6c12ad
Eaglegrant <grantman522@gmail.com>
2023-02-21 22:48:07 -0500

    not working flip/auto cube detect code

--- src/main/java/frc/robot/commands/Autos/driveAuto.java
+++ src/main/java/frc/robot/commands/Autos/driveAuto.java
@@ -10,13 +10,13 @@ import frc.robot.subsystems.intakeS;
 
   public class driveAuto extends SequentialCommandGroup {
   public driveAuto(tankDriveS drive, intakeS intake){
+    basicAuto basicAutoL = new basicAuto(drive, 5.328 , .4);
+   // otherAuto otherAutoL = new otherAuto(drive, 10, .4);
+ //   encoderDriveC encoderDriveL = new encoderDriveC(drive, 10.0);
+    shootOutC shootOutL = new shootOutC(drive,intake,2,.5);
+    flipAutoC flipAutoL = new flipAutoC(drive,.4,180);
     System.out.print("Initializing driveAuto...");
-    addCommands(
-    new shootOutC(drive,intake, 2, .5)
-  // new basicAuto(drive, 5.328 , .4)
-   //new otherAuto(drive, 10, .4)
-   //new encoderDriveC(drive, 10.0)
-   );
+    addCommands(shootOutL,basicAutoL,flipAutoL);
    //addCommands(new basicAuto(drive,5.328,.4));
 
   }
