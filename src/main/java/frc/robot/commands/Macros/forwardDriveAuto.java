4c00a664ce42af05ae1aff7f56933596ef20e6aa
coocoo <112586798+coocoo752@users.noreply.github.com>
2023-03-15 18:33:57 -0400

    commented

--- src/main/java/frc/robot/commands/Macros/forwardDriveAuto.java
+++ src/main/java/frc/robot/commands/Macros/forwardDriveAuto.java
@@ -12,8 +12,11 @@ double autoSpeed;
     Timer timeElapsed= new Timer();
     public forwardDriveAuto(tankDriveS subsystem, double desiredTime, double MotorSpeed ){
         seconds = desiredTime;
+        //when using the word "seconds" you are really refering to the desired time
         drive = subsystem;
+        //when you use the word "drive" you are refering to the subsystem tankDriveS
         autoSpeed = MotorSpeed;
+        //when using the word "autoSpeed" you are refering to the motor speed
         addRequirements(subsystem);
         
     }
@@ -24,6 +27,7 @@ double autoSpeed;
     public void execute() {
 
         if (timeElapsed.get() <= seconds) {
+            //if the time that has gone by is less than the desired time then make the robot move forward 
             
             drive.tankDrive((-autoSpeed-.01), autoSpeed);
             
@@ -31,9 +35,12 @@ double autoSpeed;
     }
     public void end() {
         drive.tankDrive(0,0);
+        //at the end of auto it makes the wheels stop moving and stops the robot from moving
         drive.runStop();
         timeElapsed.stop();
+        //stops the timer known as timeElapsed as it is not needed anymore 
         timeElapsed.reset();
+        //resets the timmer so that we can use it next match
 
     }
     }
