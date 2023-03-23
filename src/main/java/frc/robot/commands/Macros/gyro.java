839650d4de24c7d42566f36f5048e3629c5e3d67
Eaglegrant <grantman522@gmail.com>
2023-03-23 10:07:11 -0400

    Rough Gyro Crash Fix

--- src/main/java/frc/robot/commands/Macros/gyro.java
+++ src/main/java/frc/robot/commands/Macros/gyro.java
@@ -15,25 +15,27 @@ double gyroError = 0.0;
     }
     @Override
     public void initialize(){
-        drive.resetNavX();
-        while(gyroError >= 358 && gyroError  <= 2){
+        /*while(gyroError >= 358 && gyroError  <= 2){
             gyroError = drive.navx.getPitch();
             gyroError = gyroError%360;
             drive.tankDrive(-.7,.7);
             SmartDashboard.putNumber("gyro alignment",gyroError);
         }
+        */
     }
     @Override
     public void execute(){
-      //  gyroError = drive.navx.getPitch();
+        gyroError = drive.navx.getPitch();
+        SmartDashboard.putNumber(("gyro raw"),gyroError);
         gyroError = gyroError%360;
         if (gyroError >358 && gyroError<2){
         gyroError%= 30; //this is a modulus function same call as python solely exists just so it won't output anything greater than 1, gyro should keep outputting a decimal that gets closer and closer to zero, which should allow us to slow down effectively.
         drive.tankDrive(-.5*gyroError, .5*gyroError);
             SmartDashboard.putNumber("gyro alignment",gyroError);
-        }
+        }else{
+        drive.tankDrive(0,0);
         isFinished = true;
-
+        }
     }
     @Override
     public boolean isFinished(){
